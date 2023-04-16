edb03bf07abd9719a960190e53fc8035de10a62a
Kel Strother <kelstrother@gmail.com>
2023-04-16 11:32:58 -0600

    completed asynchronus lecture. created folder for fetch and async js.

--- asynchronus-JS/callback-hell/script.js
+++ asynchronus-JS/callback-hell/script.js
@@ -53,21 +53,105 @@
 // }).then((user) => console.log(user));
 
 // ? handling errors
-const getUser = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    let error = false;
+// const getUser = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let error = false;
 
-    if (!error) {
-      resolve({ name: "Apollo", age: 0 });
-    } else {
-      reject("error: something went wrong");
-    }
-  }, 1000);
+//     if (!error) {
+//       resolve({ name: "Apollo", age: 0 });
+//     } else {
+//       reject("error: something went wrong");
+//     }
+//   }, 1000);
+// });
+// getUser
+//   .then((user) => console.log(user))
+//   .catch((error) => console.log(error))
+//   //~ .finally will run whether its been resolved or rejected
+//   .finally(() => console.log("the promise has been resolved or rejected"));
+
+// console.log("hello from global scope");
+
+//~ //////////////////////////////////////
+//!   REFACTORING CALLBACK TO PROMISE   \\
+//~ //////////////////////////////////////
+
+// function getData(endpoint) {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("GET", endpoint);
+
+//     xhr.onreadystatechange = function () {
+//       if (this.readyState === 4) {
+//         if (this.status === 200) {
+//           resolve(JSON.parse(this.responseText));
+//         } else {
+//           reject("something went wrong");
+//         }
+//       }
+//     };
+//     setTimeout(() => {
+//       xhr.send();
+//     }, Math.floor(Math.random() * 3000) + 1000);
+//   });
+// }
+
+// getData("./movies.json")
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData("./actors.json");
+//   })
+//   .then((actors) => {
+//     console.log(actors);
+//     return getData("./directors.json");
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+
+  //~ //////////////////////////////////////
+//!              PROMISE.ALL()          \\
+//~ //////////////////////////////////////
+//? VERY  CLEAN WAY TO FETCH FROM MULTIPLE PLACES
+
+function getData(endpoint) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", endpoint);
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject("something went wrong");
+        }
+      }
+    };
+    setTimeout(() => {
+      xhr.send();
+    }, Math.floor(Math.random() * 3000) + 1000);
+  });
+}
+
+//~ creating a variable to hold the returned data
+const moviesPromise = getData('./movies.json');
+const actorsPromise = getData('./actors.json');
+const directorsPromise = getData('./directors.json');
+
+const dummyPromise = new Promise((resolve, reject) => {
+  resolve('Dummy Promise is resolved')
 });
-getUser
-  .then((user) => console.log(user))
-  .catch((error) => console.log(error))
-  //~ .finally will run whether its been resolved or rejected
-  .finally(() => console.log("the promise has been resolved or rejected"));
 
-console.log("hello from global scope");
+
+//~ using the .all() method on the Promise
+Promise.all([moviesPromise, actorsPromise, directorsPromise, dummyPromise])
+.then((data) => {
+  console.log(data);
+})
+.catch((error) => {
+  console.log(error);
+})
--- asynchronus-JS/callbacks/script.js
+++ asynchronus-JS/callbacks/script.js
@@ -21,24 +21,23 @@ const posts = [
 //   }, 2000)
 // }
 //? CREATE A NEW POST USING A CALLBACK (cb) //
-function createPost(post, cb) {
-  setTimeout(() => {
-    posts.push(post)
-    cb()
-  }, 2000)
-}
-
+// function createPost(post, cb) {
+//   setTimeout(() => {
+//     posts.push(post)
+//     cb()
+//   }, 2000)
+// }
 
 //! FUNCTION TO MIMIC MAKING A NETWORK REQUEST TO GET SOMETHING
-function getPosts() {
-  setTimeout(() => {
-    posts.forEach((function (post) {
-      const div = document.createElement('div');
-      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
-      document.querySelector('#posts').appendChild(div);
-    }))
-  }, 1000);
-}
+// function getPosts() {
+//   setTimeout(() => {
+//     posts.forEach((function (post) {
+//       const div = document.createElement('div');
+//       div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+//       document.querySelector('#posts').appendChild(div);
+//     }))
+//   }, 1000);
+// }
 
 //! post is created, but not added to the dom cause it took longer than getPosts ^^^
 // createPost({
@@ -49,7 +48,78 @@ function getPosts() {
 // getPosts()
 
 //? so we pass in getPosts() to the createPost when it's called.
-createPost({
-  title: "Post Three",
-  body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora quis, eaque dolorum voluptatibus aliquid dolor neque et error esse cumque.",
-}, getPosts);
+// createPost({
+//   title: "Post Three",
+//   body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora quis, eaque dolorum voluptatibus aliquid dolor neque et error esse cumque.",
+// }, getPosts);
+
+//~ //////////////////////////////////////
+//!   REFACTORING CALLBACK TO PROMISE   \\
+//~ //////////////////////////////////////
+// function createPost(post) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let error = true;
+
+//       if (!error) {
+//         posts.push(post);
+//         resolve();
+//       } else {
+//         reject("Something went wrong.");
+//       }
+//     }, 2000);
+//   });
+// }
+
+// function getPosts() {
+//   setTimeout(() => {
+//     posts.forEach(function (post) {
+//       const div = document.createElement("div");
+//       div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+//       document.querySelector("#posts").appendChild(div);
+//     });
+//   }, 1000);
+// }
+
+// function showError(error) {
+//   const h3 = document.createElement("h3");
+//   h3.innerHTML = `<strong>${error}</strong>`;
+//   document.querySelector("#posts").appendChild(h3);
+// }
+
+// createPost({
+//   title: "Post Three",
+//   body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora quis, eaque dolorum voluptatibus aliquid dolor neque et error esse cumque.",
+// })
+//   .then(getPosts)
+//   .catch(showError);
+//~ //////////////////////////////////////
+//!       PROMISE CHAINING             \\
+//~ //////////////////////////////////////
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ name: "Apollo", age: 0 });
+    } else {
+      reject("Something went wrong.");
+    }
+  }, 1000);
+});
+
+//~ ANYTHING RETURNED FROM A .THEN() CAN BE ACCESSED IN ANOTHER .THEN()
+promise
+  .then((user) => {
+    console.log(user);
+    return user.name;
+  })
+  .then((name) => {
+    console.log(name);
+    return name.length;
+  })
+  .then((nameLength) => {
+    console.log(nameLength);
+  })
+  .catch((error) => console.log(error))
+  .then(() => console.log('this will run no matter what'));
