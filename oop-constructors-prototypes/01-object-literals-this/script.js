81f8dae1e27ff27cfb4f30eb274982f1a408a56f
Kel Strother <kelstrother@gmail.com>
2023-04-21 21:53:19 -0600

    finished object literals and constructor lessons.

--- oop-constructors-prototypes/01-object-literals-this/script.js
+++ oop-constructors-prototypes/01-object-literals-this/script.js
@@ -14,3 +14,27 @@
 //! 4. POLYMORPHISM:
 //~   ----- The process of inheriting the properties and methods
 //~         from a parent class or constructor.
+
+
+//~ /////////////////////////
+//!      OBJECT LITERALS        //
+//~ /////////////////////////
+
+const rectangle = {
+  name: 'Rectangle 1',
+  width: 20,
+  height: 10,
+  area: function() {
+    return this.width * this.height;
+  }
+}
+
+const rectangle2 = {
+  name: 'Rectangle 2',
+  width: 30,
+  height: 20,
+  area: function() {
+    return this.width * this.height;
+  }
+}
+console.log(rectangle.area());
