acdd061e1d55d19a83be70892dc8aff470d12e80
Kel Strother <kelstrother@gmail.com>
2023-04-21 19:30:30 -0600

    began the OOP section.

---
+++ oop-constructors-prototypes/01-object-literals-this/script.js
@@ -0,0 +1,16 @@
+//~ /////////////////////////
+//!      4 BASIC PRINCIPLES OF OOP        //
+//~ /////////////////////////
+
+//! 1. ABSTRACTION:
+//~   ----- Hiding all but the relevant parts of an object
+//~         to reduce complexity and increase efficiency.
+//! 2. ENCAPSULATION:
+//~   ----- The process of wrapping up data and methods
+//~         into a unit such as a class or function (data hiding).
+//! 3. INHERITANCE:
+//~   ----- The process of inheriting the properties and methods
+//~         from a parent class or constructor.
+//! 4. POLYMORPHISM:
+//~   ----- The process of inheriting the properties and methods
+//~         from a parent class or constructor.
